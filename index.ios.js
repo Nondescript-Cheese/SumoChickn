e19b7454f6eae4273c04b28625ef10cc1f02c203
Michael Cheung <michaelc1993@gmail.com>
2016-03-19 14:30:16 -0700

    create app

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+import React, {
+  AppRegistry,
+  Component,
+  Text
+} from 'react-native';
+
+class challengeApp extends Component {
+  render() {
+    return (
+    <Text>HELLO WORLD</Text>
+    );
+  }
+}
+
+AppRegistry.registerComponent('challengeApp', () => challengeApp);
