fba6b186e8175c8ce29cbd8a9a1017d1b7be2b2d
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-03-22 16:10:11 -0700

    Create functionality to filter by open/closed challenges

--- app/containers/ChallengeList.js
+++ app/containers/ChallengeList.js
@@ -1,15 +1,27 @@
 import { connect } from 'react-redux'
 import MyChallenges from '../components/MyChallengesComponent.js'
+import { setChallengesViewStatus } from '../actions'
+
+const filterView = (challenges, challengesViewStatus) => {
+  switch (challengesViewStatus) {
+    case true :
+      return challenges.filter(c => c.completed)
+    case false:
+      return challenges.filter(c=> !c.completed)
+  }
+}
+
 
 const mapStateToProps = (state) => {
   return {
-    visibleChallenges: state.challenges.challengeList
+    visibleChallenges: filterView(state.challenges.ChallengeList, state.challengesViewStatus),
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-
+    onClick: bindActionCreators(setChallengesViewStatus, dispatch)
+    }
   }
 }
 const ChallengeList = connect(
