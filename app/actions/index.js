10f914693aea3cae9c67fa6a5a429924a0ae8fc2
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-03-21 20:45:31 -0700

    Create Action type for postingchallenge boolean

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,8 +1,9 @@
-let challengeID = 0
-export const addChallenge = (text) => {
+
+
+export const ChallengePosting = (challenge) => {
   return {
-    type: 'ADD_CHALLENGE',
-    id: challengeID++,
-    text
+    type: CHALLENGE_POSTING,
+    payload: challenge
   }
 }
+
