d6cf295941896d4a981439bb498a706d78b29326
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-03-22 16:14:30 -0700

    Update with info and connect to dumb component

--- app/containers/SendChallenge.js
+++ app/containers/SendChallenge.js
@@ -1,3 +1,25 @@
 import { connect } from 'react-redux'
-import CreateChallengesfrom '../components/CreateChallengesComponent'
+import CreateChallenges from '../components/CreateChallengesComponent'
 import { setChallengesViewStatus } from '../actions'
+
+const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    
+  }
+}
+
+
+const SendChallenge = connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(CreateChallenges)
+
+
+//TODO: UPDATE IN APP.JS
+export default SendChallenge
