fde0a8ce153fa6443a0f22151287876601deebb0
SteffenBerlin <Steffen.Baumgarten@gmx.de>
2016-03-29 23:46:15 -0700

    finished view with fakedata, built in voting buttons

--- app/components/Proof.js
+++ app/components/Proof.js
@@ -2,13 +2,37 @@ import React, {
   Text,
   StyleSheet,
   View,
-  TouchableHighlight
+  TouchableHighlight,
+  Image
 } from 'react-native'
 
-const Proof = ({createdBy, challengeText, proofUrl, points}) => {
+const Proof = ({createdBy, userChallenged, challengeText, proofUrl, points}) => {
   return (
     <View style={styles.listItem}>
-      <Text style={styles.challengeText}>{challengeText}</Text>
+      <View style={styles.itemHeadline}>
+        <Text style={styles.challengeText}>{createdBy} challenged {userChallenged}:</Text>
+      </View>
+      <View style={styles.itemBody}>
+        <View style={styles.itemHeadBody}>
+          <View>
+            <Text>{challengeText}</Text>
+          </View>
+          <View style={styles.buttons}>
+            <TouchableHighlight>
+              <Text>YES</Text>
+            </TouchableHighlight>
+            <TouchableHighlight>
+              <Text>NO</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+        <TouchableHighlight style={styles.thumbnail}>
+          <Image source={{uri: proofUrl}} style = {{width: 350, height: 350}}  resizeMode={Image.resizeMode.contain} />
+        </TouchableHighlight>
+        <View style={styles.explanation}>
+          <Text>{userChallenged} gets {points} Points unless more than X people vote NO in the next XX:XX</Text>
+        </View>
+      </View>
     </View>
   )
 }
@@ -20,10 +44,28 @@ var styles = StyleSheet.create({
   listItem: {
     borderColor: 'blue',
     borderWidth: 2,
-    padding: 7.5
+    margin: 7.5
+  },
+  itemHeadline: {
+    borderColor: 'green',
+    borderWidth: 2,
+  },
+  itemHeadBody: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  itemBody: {
+    borderColor: 'pink',
+    borderWidth: 1
   },
   challengeText: {
-    fontSize: 30 
+    fontSize: 20 
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+  thumbnail: {
+    borderColor: 'lightblue'
   }
 })
 
