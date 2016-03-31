a0e2435c38141d4e080e0ae4b83b2cde23e83270
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-03-30 17:18:07 -0700

    Implement pull to refresh for Proof page and update styling to follow rest of the app format

--- app/components/ProofFeedComponent.js
+++ app/components/ProofFeedComponent.js
@@ -4,13 +4,16 @@ import React, {
  ListView,
  StyleSheet,
  TouchableHighlight,
- ScrollView
+ ScrollView,
+ RefreshControl,
+ TouchableOpacity,
+ Image
 } from 'react-native'
 
 import Proof from './Proof'
 
 
-const ProofFeedComponent = ({currentUserId, allClosedChallenges, getClosedChallenges, voteOnChallenge}) => {
+const ProofFeedComponent = ({currentUserId, allClosedChallenges, getClosedChallenges, voteOnChallenge, refreshingClosedChallenges}) => {
   console.log('these are the challenges', allClosedChallenges)
 
   let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
@@ -18,16 +21,67 @@ const ProofFeedComponent = ({currentUserId, allClosedChallenges, getClosedChalle
   let dataSource = ds.cloneWithRows(allClosedChallenges)
 
   return (
-    <ScrollView>
+    <View style={styles.container}>
+    <View style={styles.header}>
+    <TouchableOpacity onPress={()=>Alert.alert("Check out all the completed challenges and vote on them! Pull down to get the latest challenges")}>
+      <Image source={{uri: 'https://s3-us-west-1.amazonaws.com/challengrproof/Drawing-layerExport+(3).jpeg'}} style={{width:50, height:50}} resizeMode={Image.resizeMode.contain} />
+    </TouchableOpacity>
+    </View>
+
+
+    <View style={styles.body}>
+      <View style={styles.titleBar}>
+         <Text style ={styles.titleText}>
+           Completed Challenge Feed
+         </Text>
+     </View>
+    <ScrollView 
+    refreshControl={
+          <RefreshControl
+            refreshing={refreshingClosedChallenges}
+            onRefresh={()=>{getClosedChallenges(1, allClosedChallenges.length+10)}}
+            tintColor="#ff0000"
+            title={"Loading newly completed challenges"}
+            colors={['#ff0000', '#00ff00', '#0000ff']}
+            progressBackgroundColor="#ffff00"
+          />
+      }>
     	<ListView
         dataSource={dataSource}
         renderRow={(row) => <Proof {...row} currentUserId={currentUserId} listLength={allClosedChallenges.length} voteOnChallenge={voteOnChallenge}/>}
       />
-      <TouchableHighlight onPress={() => {getClosedChallenges(1, allClosedChallenges.length+10)}}>
-        <Text>fetch Closed Challenges</Text>
-      </TouchableHighlight>
     </ScrollView>
+    </View>
+    </View>
     )
 }
+
+var styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   backgroundColor: 'white'
+ },
+ header: {
+   flex: 0.1,
+   alignItems: 'center',
+   justifyContent: 'center'
+ },
+ body: {
+   flex: 0.6,
+   backgroundColor: '#f5f5f5'
+ },
+ titleText:{
+  fontSize: 20,
+  fontWeight: "600",
+  color: 'white'
+ },
+  titleBar: {
+   flex: 0.1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   marginBottom: 5,
+   backgroundColor: '#ff005f'
+ },
+})
 
 export default ProofFeedComponent
