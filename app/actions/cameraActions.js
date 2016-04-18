18678cc0e00f1a2791934d07da7a40e9a4a59554
Michael Cheung <michaelc1993@gmail.com>
2016-04-18 10:38:29 -0700

    remove console logs and comments in cameraActions

--- app/actions/cameraActions.js
+++ app/actions/cameraActions.js
@@ -1,14 +1,12 @@
 export const UPLOADING_PICTURE = 'UPLOADING_PICTURE'
 export const UPLOADED_PICTURE = 'UPLOADED_PICTURE'
 export const CAMERA_CHALLENGE_ID = 'CAMERA_CHALLENGE_ID'
-// import { polyfill } from 'es6-promise'; polyfill();
 
 let RNUploader = require('NativeModules').RNUploader
 let xmlConvert = require("node-xml2json");
 
 
 export const postingPicture = (challengeId, picture) => {
-  console.log('PICTURE', picture)
 
   return (dispatch) => {
     var upload = new Promise((resolve, reject) => {
@@ -19,20 +17,16 @@ export const postingPicture = (challengeId, picture) => {
         const status = res.status
         const response = xmlConvert.parser(res.data)
         resolve(response)
-        console.log('logging with', response)
-        console.log('STATUS', status)
       })
     })
 
     upload.then((data) => {
       dispatch(postPicture(challengeId, data.postresponse.location))
-      // console.log('WORK PLEASE', data)
     })
   }
 }
 
 export const postPicture = (challengeId, awsUrl) => {
-  console.log('I WORKD', awsUrl)
   return (dispatch) => {
     return fetch('http://159.203.239.224:3000/addPhoto/'+challengeId, {
       method: 'PUT',
@@ -54,7 +48,6 @@ export const postPicture = (challengeId, awsUrl) => {
 }
 
 export const cameraChallengeId = (challengeId) => {
-  console.log("IN THE CHAMERA CHALLENGE")
   return {
     type: CAMERA_CHALLENGE_ID,
     id: challengeId
@@ -65,11 +58,9 @@ export const getChallengePhoto = (challengeId) => {
   return (dispatch)=>{
     return fetch('http://159.203.239.224:3000/getPhoto/'+challengeId)
     .then((response)=>{
-      console.log("THIS IS THE REPONSE FROM GET PHOTO", response)
       return response.text()
     })
     .then((photoURL)=>{
-      console.log("GOT SOMETHING FROM DATABSE", photoURL)
       return photoURL
     })
     .catch((error)=>{
